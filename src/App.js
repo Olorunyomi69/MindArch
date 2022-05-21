5dd003ef9795e30ebc0e049fdb075adf13fe88f6
Olorunyomi69 <isaacolorunyomi69@gmail.com>
2022-05-21 13:20:04 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import Header from './Components/Header';
 import SearchBox from './Components/SearchBox';
 
+
 function App() {
   return (
     <div>
       <Header/>
       <SearchBox/>
+     
     </div>
     
   );
